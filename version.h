63d73cded6b4006f01f8b701f68c6970a6f109d6
Oscar Saleta Reig <oscar.saleta@gmail.com>
2016-09-26 09:54:51 +0200

    Added version header v3.2

---
+++ version.h
@@ -0,0 +1,14 @@
+/************************************************************************
+
+    P4 Distribution
+
+    Beneath, you find the current version that is located in the
+    /p4, /separatrice, /lyapunov, /reduce and /maple subdirectories.
+
+    Do not delete this file; it is needed during compilation of
+    the source files.
+
+*************************************************************************/
+
+#define VERSION         "3.2"
+#define VERSIONDATE     "September, 2016"
