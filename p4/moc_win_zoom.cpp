f439392d5571d54c37245e95a60ce7d5183af00a
Oscar Saleta Reig <oscar.saleta@gmail.com>
2016-11-08 13:25:21 +0100

    project under LGPLv3 for open source Qt

--- p4/moc_win_zoom.cpp
+++ p4/moc_win_zoom.cpp
@@ -1,3 +1,47 @@
+/*  P4 (Polynomial Planar Phase Portraits) GUI SOURCE CODE
+ *  Software to study polynomial planar differential systems and represent
+ *  their phase portrait in several spaces, such as Poincaré sphere. 
+ *  URL: http://github.com/oscarsaleta/P4-src-gui
+ *
+ *  Copyright (C) 1996-2016  J.C. Artés, C. Herssens, P. De Maesschalck,
+ *                           F. Dumortier, J. Llibre, O. Saleta
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Lesser General Public License as published
+ *  by the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/*  P4 (Polynomial Planar Phase Portraits) GUI SOURCE CODE
+ *  Software to study polynomial planar differential systems and represent
+ *  their phase portrait in several spaces, such as Poincaré sphere. 
+ *  URL: http://github.com/oscarsaleta/P4-src-gui
+ *
+ *  Copyright (C) 1996-2016  J.C. Artés, C. Herssens, P. De Maesschalck,
+ *                           F. Dumortier, J. Llibre, O. Saleta
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 /****************************************************************************
 ** Meta object code from reading C++ file 'win_zoom.h'
 **
