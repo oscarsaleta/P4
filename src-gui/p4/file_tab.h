d8c79970d8ee1a7a30047b73c781edfa19371e3c
Oscar Saleta <oscar.saleta@gmail.com>
2017-11-15 13:00:27 +0100

    add todo comment

--- src-gui/p4/file_tab.h
+++ src-gui/p4/file_tab.h
@@ -381,7 +381,7 @@ class QVFStudy : public QObject
     std::vector<curves> curve_vector_;
     // isoclines
     std::vector<isoclines> isocline_vector_;
-    // TODO: vector for orbits
+    // TODO: vector for orbits (hem de poder incloude pcoord, fem struct?)
     QVector<orbits_points> orbits_vector_;
 
     // limit cycles
