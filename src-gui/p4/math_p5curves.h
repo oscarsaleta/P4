56bddb1282452e8981d50315a80010fc4cf990d9
Oscar Saleta Reig <oscar.saleta@gmail.com>
2018-01-10 15:30:01 +0100

    math_p5curves header, only has one function

---
+++ src-gui/p4/math_p5curves.h
@@ -0,0 +1,23 @@
+/*  This file is part of P4
+ *
+ *  Copyright (C) 1996-2018  J.C. Artés, P. De Maesschalck, F. Dumortier
+ *                           C. Herssens, J. Llibre, O. Saleta, J. Torregrosa
+ *
+ *  P4 is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Lesser General Public License as published
+ *  by the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+ 
+#include "file_tab.h"
+
+bool readCurvePoints(FILE *fp, std::vector<p4orbits::orbits_points> &psep,
+                     int index);
