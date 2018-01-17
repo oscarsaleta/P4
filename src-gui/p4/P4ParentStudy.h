c4040106e46400d17f7610d1010bc69bd7e1d10f
Oscar Saleta <oscar.saleta@gmail.com>
2018-01-17 15:45:33 +0100

    WIP: selected_*_ members should be pointers to elements of vectors without the selected_ bit in the name

--- src-gui/p4/P4ParentStudy.h
+++ src-gui/p4/P4ParentStudy.h
@@ -29,7 +29,7 @@ class P4VFStudy;
 
 class P4ParentStudy : public QObject
 {
-  public:
+   public:
     // Constructor and destructor
     P4ParentStudy();
     ~P4ParentStudy();
@@ -43,28 +43,29 @@ class P4ParentStudy : public QObject
     std::vector<p4curveRegions::curveResult> curves_result_;
 
     int typeofstudy_;
-    int typeofview_;                // TYPEOFVIEW_PLANE or TYPEOFVIEW_SPHERE
-    bool plotVirtualSingularities_; // true or false
+    int typeofview_;                 // TYPEOFVIEW_PLANE or TYPEOFVIEW_SPHERE
+    bool plotVirtualSingularities_;  // true or false
     int p_;
     int q_;
-    bool plweights_; // true if p<>1 or q<>1; false if p=q=1
+    bool plweights_;  // true if p<>1 or q<>1; false if p=q=1
     double config_projection_;
 
-    double double_p_;                  // shortcuts: = (double)p
-    double double_q_;                  // = (double)q
-    double double_p_plus_q_;           // = (double)(p+q)
-    double double_p_minus_1_;          // = (double)(p-1)
-    double double_q_minus_1_;          // = (double)(q-1)
-    double double_q_minus_p_;          // = (double)(q-p)
-    double xmin_, xmax_, ymin_, ymax_; // in case of local study
+    double double_p_;                   // shortcuts: = (double)p
+    double double_q_;                   // = (double)q
+    double double_p_plus_q_;            // = (double)(p+q)
+    double double_p_minus_1_;           // = (double)(p-1)
+    double double_q_minus_1_;           // = (double)(q-1)
+    double double_q_minus_p_;           // = (double)(q-p)
+    double xmin_, xmax_, ymin_, ymax_;  // in case of local study
+
+    // curves
+    std::vector<p4curves::curves> curve_vector_;
 
     // limit cycles and orbits
-
     std::vector<p4orbits::orbits> first_lim_cycle_;
     std::vector<p4orbits::orbits> first_orbit_;
 
     // run-time when plotting
-
     p4orbits::orbits *current_orbit_;
     p4orbits::orbits *current_lim_cycle_;
 
@@ -72,13 +73,15 @@ class P4ParentStudy : public QObject
     std::vector<p4singularities::saddle> selected_saddle_point_;
     std::vector<p4singularities::semi_elementary> selected_se_point_;
     std::vector<p4singularities::degenerate> selected_de_point_;
-    std::vector<p4singularities::sep> selected_sep_;
-    std::vector<p4singularities::blow_up_points> selected_de_sep_;
+    std::vector<p4blowup::sep> selected_sep_;
+    //std::vector<p4blowup::blow_up_points> selected_de_sep_;
+    std::vector<p4blowup::blow_up_points> de_seps_;
+    std::shared_ptr<p4blowup::blow_up_points> selected_de_sep_;
     int selected_sep_vfindex_;
 
     double config_currentstep_;
     bool config_dashes_;
-    bool config_kindvf_; // true for original VF, false for reduced
+    bool config_kindvf_;  // true for original VF, false for reduced
     int config_lc_value_;
     int config_lc_numpoints_;
     double config_hma_;
@@ -89,9 +92,9 @@ class P4ParentStudy : public QObject
     int config_intpoints_;
 
     // Methods
-    //void deleteOrbitPoint(P4ORBIT p);
-    //void deleteLimitCycle(p4orbits::orbits *);
-    //void deleteOrbit(p4orbits::orbits *);
+    // void deleteOrbitPoint(P4ORBIT p);
+    // void deleteLimitCycle(p4orbits::orbits *);
+    // void deleteOrbit(p4orbits::orbits *);
 
     bool readPiecewiseData(FILE *);
     void examinePositionsOfSingularities();
@@ -100,8 +103,6 @@ class P4ParentStudy : public QObject
     void reset();
     void setupCoordinateTransformations();
     bool readSeparatingCurve(FILE *);
-
-    void resetCurveInfo(int);
 
     void clearVFs();
 
