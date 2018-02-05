d2e0ad433ccd2f75bb0ed434bccdcaf359df5a2d
Oscar Saleta <oscar.saleta@gmail.com>
2018-02-05 08:23:55 +0100

    P4ParentStudy.h: changed first_lim_cycle_ and current_lim_cycle_ for lim_cycles_, and will use front() and back() when necessary

--- src-gui/p4/P4ParentStudy.h
+++ src-gui/p4/P4ParentStudy.h
@@ -28,7 +28,7 @@ class P4VFStudy;
 
 class P4ParentStudy : public QObject
 {
-  public:
+   public:
     // Constructor and destructor
     P4ParentStudy();
     //~P4ParentStudy();
@@ -42,26 +42,27 @@ class P4ParentStudy : public QObject
     std::vector<p4curveRegions::curveResult> curves_result_;
 
     int typeofstudy_;
-    int typeofview_;                // TYPEOFVIEW_PLANE or TYPEOFVIEW_SPHERE
-    bool plotVirtualSingularities_; // true or false
+    int typeofview_;                 // TYPEOFVIEW_PLANE or TYPEOFVIEW_SPHERE
+    bool plotVirtualSingularities_;  // true or false
     int p_;
     int q_;
-    bool plweights_; // true if p<>1 or q<>1; false if p=q=1
+    bool plweights_;  // true if p<>1 or q<>1; false if p=q=1
     double config_projection_;
 
-    double double_p_;                  // shortcuts: = (double)p
-    double double_q_;                  // = (double)q
-    double double_p_plus_q_;           // = (double)(p+q)
-    double double_p_minus_1_;          // = (double)(p-1)
-    double double_q_minus_1_;          // = (double)(q-1)
-    double double_q_minus_p_;          // = (double)(q-p)
-    double xmin_, xmax_, ymin_, ymax_; // in case of local study
+    double double_p_;                   // shortcuts: = (double)p
+    double double_q_;                   // = (double)q
+    double double_p_plus_q_;            // = (double)(p+q)
+    double double_p_minus_1_;           // = (double)(p-1)
+    double double_q_minus_1_;           // = (double)(q-1)
+    double double_q_minus_p_;           // = (double)(q-p)
+    double xmin_, xmax_, ymin_, ymax_;  // in case of local study
 
     // curves
     std::vector<p4curves::curves> curve_vector_;
 
-    // limit cycles and orbits
-    std::vector<p4orbits::orbits> first_lim_cycle_;
+    // FIXME segurament puc canviar first_lim_cycle per lim_cycles.front() i
+    // current per .back() limit cycles and orbits
+    std::vector<p4orbits::orbits> lim_cycles_; // FIXME canviar ocurrències
     std::vector<p4orbits::orbits> first_orbit_;
 
     // run-time when plotting FIXME probably same problem as selected_ variables
@@ -85,18 +86,18 @@ class P4ParentStudy : public QObject
 
     int selected_sep_vfindex_;
 
-    double config_currentstep_; // current step size (during integration)
-    bool config_dashes_;        // line style (dashes or points)
-    bool config_kindvf_;        // true for original VF, false for reduced
-    int config_lc_value_;       // number of orbits in the limit cycle window
-    int config_lc_numpoints_;   // number of points in the limit cycle window
-    double config_hma_;         // maximum step size
-    double config_hmi_;         // minimum step size
-    double config_branchhmi_;   // minimum step size near branches of separating
-                                // curves
-    double config_step_;        // step size
-    double config_tolerance_;   // tolerance
-    int config_intpoints_;      // number of points to integrate
+    double config_currentstep_;  // current step size (during integration)
+    bool config_dashes_;         // line style (dashes or points)
+    bool config_kindvf_;         // true for original VF, false for reduced
+    int config_lc_value_;        // number of orbits in the limit cycle window
+    int config_lc_numpoints_;    // number of points in the limit cycle window
+    double config_hma_;          // maximum step size
+    double config_hmi_;          // minimum step size
+    double config_branchhmi_;  // minimum step size near branches of separating
+                               // curves
+    double config_step_;       // step size
+    double config_tolerance_;  // tolerance
+    int config_intpoints_;     // number of points to integrate
 
     // Methods
     // void deleteOrbitPoint(P4ORBIT p);
