744fb324a806991b77a2c4c156db45116731ee4a
Oscar Saleta <oscar.saleta@gmail.com>
2018-02-06 15:51:27 +0100

    math_separatrice.h: QWinSphere changed to P4WinSphere

--- src-gui/p4/math_separatrice.h
+++ src-gui/p4/math_separatrice.h
@@ -20,41 +20,41 @@
 #ifndef MATH_SEPARATRICE_H
 #define MATH_SEPARATRICE_H
 
-class QWinSphere;
+class P4WinSphere;
 struct orbits_points;
 struct transformations;
 struct sep;
 struct term2;
 struct term3;
 
-extern void (*change_epsilon)(std::shared_ptr<QWinSphere>, double);
-extern void (*start_plot_sep)(std::shared_ptr<QWinSphere>);
-extern void (*cont_plot_sep)(std::shared_ptr<QWinSphere>);
-extern void (*plot_next_sep)(std::shared_ptr<QWinSphere>);
-extern void (*select_next_sep)(std::shared_ptr<QWinSphere>);
+extern void (*change_epsilon)(std::shared_ptr<P4WinSphere>, double);
+extern void (*start_plot_sep)(std::shared_ptr<P4WinSphere>);
+extern void (*cont_plot_sep)(std::shared_ptr<P4WinSphere>);
+extern void (*plot_next_sep)(std::shared_ptr<P4WinSphere>);
+extern void (*select_next_sep)(std::shared_ptr<P4WinSphere>);
 
-void start_plot_saddle_sep(std::shared_ptr<QWinSphere>);
-void cont_plot_saddle_sep(std::shared_ptr<QWinSphere>);
-void plot_next_saddle_sep(std::shared_ptr<QWinSphere>);
-void select_next_saddle_sep(std::shared_ptr<QWinSphere>);
-void change_epsilon_saddle(std::shared_ptr<QWinSphere>, double);
+void start_plot_saddle_sep(std::shared_ptr<P4WinSphere>);
+void cont_plot_saddle_sep(std::shared_ptr<P4WinSphere>);
+void plot_next_saddle_sep(std::shared_ptr<P4WinSphere>);
+void select_next_saddle_sep(std::shared_ptr<P4WinSphere>);
+void change_epsilon_saddle(std::shared_ptr<P4WinSphere>, double);
 
-void start_plot_se_sep(std::shared_ptr<QWinSphere>);
-void cont_plot_se_sep(std::shared_ptr<QWinSphere>);
-void plot_next_se_sep(std::shared_ptr<QWinSphere>);
-void select_next_se_sep(std::shared_ptr<QWinSphere>);
-void change_epsilon_se(std::shared_ptr<QWinSphere>, double);
+void start_plot_se_sep(std::shared_ptr<P4WinSphere>);
+void cont_plot_se_sep(std::shared_ptr<P4WinSphere>);
+void plot_next_se_sep(std::shared_ptr<P4WinSphere>);
+void select_next_se_sep(std::shared_ptr<P4WinSphere>);
+void change_epsilon_se(std::shared_ptr<P4WinSphere>, double);
 
-void start_plot_de_sep(std::shared_ptr<QWinSphere>);
-void cont_plot_de_sep(std::shared_ptr<QWinSphere>);
-void plot_next_de_sep(std::shared_ptr<QWinSphere>);
-void select_next_de_sep(std::shared_ptr<QWinSphere>);
-void change_epsilon_de(std::shared_ptr<QWinSphere>, double);
+void start_plot_de_sep(std::shared_ptr<P4WinSphere>);
+void cont_plot_de_sep(std::shared_ptr<P4WinSphere>);
+void plot_next_de_sep(std::shared_ptr<P4WinSphere>);
+void select_next_de_sep(std::shared_ptr<P4WinSphere>);
+void change_epsilon_de(std::shared_ptr<P4WinSphere>, double);
 
-void plot_all_sep(std::shared_ptr<QWinSphere> spherewnd);
-void draw_sep(std::shared_ptr<QWinSphere> spherewnd,
+void plot_all_sep(std::shared_ptr<P4WinSphere> spherewnd);
+void draw_sep(std::shared_ptr<P4WinSphere> spherewnd,
               std::vector<p4orbits::orbits_points> sep);
-void draw_selected_sep(std::shared_ptr<QWinSphere> spherewnd,
+void draw_selected_sep(std::shared_ptr<P4WinSphere> spherewnd,
                        std::vector<p4orbits::orbits_points> sep, int color);
 
 int findSepColor2(term2 *f, int type, double y[2]);
@@ -69,7 +69,7 @@ void integrate_lyapunov_sep(double p0, double p1, double p2, double *pcoord,
 
 int change_type(int type);
 
-orbits_points *plot_separatrice(std::shared_ptr<QWinSphere> spherewnd,
+orbits_points *plot_separatrice(std::shared_ptr<P4WinSphere> spherewnd,
                                 double x0, double y0, double a11, double a12,
                                 double a21, double a22, double epsilon,
                                 sep *sep1, orbits_points **orbit,
