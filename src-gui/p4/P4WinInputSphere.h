619130e3bb7d3e65de4892659183199a8b9db265
Oscar Saleta <oscar.saleta@gmail.com>
2018-02-12 17:08:48 +0100

    P4WinInputSphere.h: (wip) started file

---
+++ src-gui/p4/P4WinInputSphere.h
@@ -0,0 +1,36 @@
+/*  This file is part of P4
+ *
+ *  Copyright (C) 1996-2018  J.C. Artés, P. De Maesschalck, F. Dumortier
+ *                           C. Herssens, J. Llibre, O. Saleta, J. Torregrosa
+ *
+ *  P4 is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Lesser General Public License as published
+ *  by the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#pragma once
+
+#include <QWidget>
+
+class P4CurvesDlg;
+class QLabel;
+class QPaintEvent;
+
+class P4WinInputSphere : public QWidget
+{
+    Q_OBJECT
+
+   public:
+    P4WinInputSphere(P4SeparatingCurvesDlg *, QLabel *);
+
+    void paintEvent(QPaintEvent *);
+}
