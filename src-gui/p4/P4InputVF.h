9d96600037dfc271ab98129ccee885733c0117ae
Oscar Saleta <oscar.saleta@gmail.com>
2018-02-21 12:21:31 +0100

    P4InputVF.h: (wip) header is almost adapted, will need tweaking

--- src-gui/p4/P4InputVF.h
+++ src-gui/p4/P4InputVF.h
@@ -70,15 +70,17 @@ class P4InputVF : public QObject
 
    public:
     P4InputVF();
-    //~P4InputVF();
 
-    // MEMBER VARIABLES
+    //////////////////////
+    // MEMBER VARIABLES //
+    //////////////////////
 
     QString filename_;
 
     int symbolicpackage_;  // 0 for reduce, 1 for maple
     int typeofstudy_;      // 0, 1, 2, 3 = all, inf, fin, one
 
+    // MAPLE EXECUTION PARAMETERS
     std::vector<bool> numeric_;
     std::vector<int> precision_;
     std::vector<int> precision0_;
@@ -88,54 +90,142 @@ class P4InputVF : public QObject
     std::vector<int> numericlevel_;
     std::vector<int> maxlevel_;
     std::vector<int> weakness_;
-
     QString x0_;
     QString y0_;
     int p_;
     int q_;
 
+    // TEXT VECTOR FIELDS
     std::vector<QString> xdot_;
     std::vector<QString> ydot_;
     std::vector<QString> gcf_;
-    // curve_ is the original, curves_ is P5. There is only one curve regardless
-    // of how many regions we have
-    QString arbitraryCurve_;  // TODO: change for arbitraryCurve_
+
+    // ARBITRARY CURVE (one for all)
+    QString arbitraryCurve_;
+
+    // ISOCLINES (one per VF)
     std::vector<QString> isoclines_;
+
+    // EVALUATION VARIABLES
     QString evalFile_;
     QString evalFile2_;
-
-    std::unique_ptr<QWidget> outputWindow_;
     std::unique_ptr<QProcess> evalProcess_;
+
+    // QT GUI ELEMENTS FIXME need to be public?
+    std::unique_ptr<QWidget> outputWindow_;
     std::unique_ptr<QTextEdit> processText_;
     std::unique_ptr<QPushButton> terminateProcessButton_;
     std::unique_ptr<QPushButton> clearProcessButton_;
 
-    // parameters list
-    int numparams_;
-    QString parlabel_[MAXNUMPARAMS];
-    std::vector<std::vector<QString>> parvalue_;
-
-    // piecewise variables
-    int numVF_;
-    int numSeparatingCurves_;
-
-    bool changed_;        // set when data needs to be saved
-    bool evaluated_;      // set when data has been evaluated
-    bool evaluating_;     // set while evaluating
-    bool cleared_;        // initial state, when records are clear
-    bool evaluatinggcf_;  // true when evaluation is of GCF kind
-    bool evaluatingCurve_;
-    bool evaluatingIsoclines_;
-    bool processfailed_;    // true when process failed;
-    QString processError_;  // only relevant when processfailed=true
-
+    // P4 GUI ELEMENTS FIXME need to be shared_ptr?
     std::unique_ptr<P4FindDlg> findDlg_;
     std::unique_ptr<P4GcfDlg> gcfDlg_;
-    std::unique_ptr<P4ArbitraryCurveDlg> curveDlg_;
+    std::unique_ptr<P4ArbitraryCurveDlg> arbitraryCurveDlg_;
     std::unique_ptr<QIsoclinesDlg> isoclinesDlg_;
 
-    // MEMBER FUNCTIONS
+    // PARAMETER LIST
+    int numparams_;
+    // list of parameter names
+    QString parlabel_[MAXNUMPARAMS];
+    // a list of parameter values for every VF (thus vector of vectors)
+    std::vector<std::vector<QString>> parvalue_;
 
+    // PIECEWISE VARIABLES: vector fields
+    // indicates length of P4ParentStudy::vf_ (not really necessary but too much
+    // work to get rid of atm)
+    int numVF_;
+    // same but for this->vfRegions_
+    int numVFRegions_;
+    std::vector<p4InputVFRegions::vfRegion> vfRegions_;
+
+    // PIECEWISE VARIABLES: separating curves
+    // same as above but for this->separatingCurves_
+    int numSeparatingCurves_;
+    std::vector<QString> separatingCurves_;
+    std::vector<int> numPointsSeparatingCurve_;
+    // same as above but for this->curveRegions_
+    int numCurveRegions_;
+    std::vector<p4InputVFRegions::curveRegion> curveRegions_;
+
+    // PIECEWISE VARIABLES: selected regions
+    int numSelected_;
+    std::vector<int> selected_;
+
+    // STATUS VARIABLES
+    // set when data needs to be saved
+    bool changed_;
+    // set when data has been evaluated
+    bool evaluated_;
+    // set while evaluating
+    bool evaluating_;
+    // initial state, when records are clear
+    bool cleared_;
+    // true when evaluation is of GCF kind
+    bool evaluatinggcf_;
+    // true when evaluation is of arbitrary curve kind
+    bool evaluatingArbitraryCurve_;
+    // true when evaluation is of isocline kind
+    bool evaluatingIsoclines_;
+    // true when evaluation is of separating curve kind
+    bool evaluatingPiecewiseConfig_;
+    // true when process failed;
+    bool processFailed_;
+    // only relevant when processFailed_=true
+    QString processError_;
+
+    //////////////////////
+    // MEMBER FUNCTIONS //
+    //////////////////////
+
+    // MANAGE VECTOR FIELDS
+    void addVectorField();
+    void deleteVectorField(int);
+
+    // MANAGE SEPARATING CURVES
+    void addSeparatingCurve();
+    void deleteSeparatingCurve(int);
+    void resampleCurve(int);
+
+    // GET VECTOR FIELD INDEX
+    int getVFIndex_R2(const double *);
+    int getVFIndex_U1(const double *);
+    int getVFIndex_U2(const double *);
+    int getVFIndex_V1(const double *);
+    int getVFIndex_V2(const double *);
+    int getVFIndex_UU1(const double *);
+    int getVFIndex_UU2(const double *);
+    int getVFIndex_VV1(const double *);
+    int getVFIndex_VV2(const double *);
+    int getVFIndex_sphere(const double *);
+    int getVFIndex_psphere(const double *);
+    int getVFIndex_plsphere(const double *);
+    int getVFIndex_cyl(const double *);
+
+    // MANAGE REGIONS
+    void markVFRegion(int, const double *);
+    void unmarkVFRegion(int, const double *);
+    void markCurveRegion(int, const double *);
+    void unmarkCurveRegion(int, const double *);
+    void clearVFMarks();
+    void clearCurveMarks();
+    bool isCurvePointDrawn(int, const double *);
+
+    void resampleGcf(int);
+
+    // COMMON ELEMENTS BETWEEN VFs
+    bool hasCommonString(const std::vector<QString> &);
+    bool hasCommonInt(const std::vector<int> &);
+    bool hasCommonBool(const std::vector<bool> &);
+    QString commonString(const std::vector<QString> &);
+    int commonInt(const std::vector<int> &);
+    bool commonBool(const std::vector<bool> &);
+    QString commonParValue(int);
+    void setCommonString(std::vector<QString> &, QString);
+    void setCommonInt(std::vector<int> &, int);
+    void setCommonBool(std::vector<bool> &, bool);
+    void setCommonParValue(int, QString);
+
+    // FILENAMES
     QString getfilename() const;             // filename.inp
     QString getbarefilename() const;         // filename
     QString getfilename_finresults() const;  // filename_fin.res
@@ -143,12 +233,8 @@ class P4InputVF : public QObject
     QString getfilename_fintable() const;    // filename_fin.tab
     QString getfilename_inftable() const;    // filename_inf.tab
     QString getfilename_vectable() const;    // filename_vec.tab
-    QString getfilename_gcf() const;  // filename_gcf.tab (temporary file)
-    // QString getfilename_gcfresults() const; // filename_gcf.res (temp file,
-    // only for reduce)
-    QString getreducefilename() const;  // filename.red
-    QString getmaplefilename() const;   // filename.txt
-    QString getrunfilename() const;     // filename.run
+    QString getfilename_gcf() const;   // filename_gcf.tab (temporary file)
+    QString getmaplefilename() const;  // filename.txt
     // curve filenames
     QString getfilename_curvetable() const;   // filename_veccurve.tab
     QString getfilename_curve() const;        // filename_curve.tab (tmp file)
@@ -157,18 +243,25 @@ class P4InputVF : public QObject
     QString getfilename_isoclinestable() const;  // filename_vecisocline.tab
     QString getfilename_isoclines() const;  // filename_isocline.tab (tmp file)
     QString getPrepareIsoclinesFileName() const;  // filename_isocline_prep.txt
+    // check if a file exists
+    static bool fileExists(QString);
 
     void getDataFromDlg();
 
+    // ACTIONS
     bool load();
     bool save();
     void reset(int);
     bool checkevaluated();
 
-    static bool fileExists(QString);
-
+    // PARAMETER PREPARATION
     void prepareMapleParameters(QTextStream &);
     void prepareMapleVectorField(QTextStream &);
+    void prepareMaplePiecewiseConfig(QTextStream &);
+    // NOTE: PrepareMapleCurves a p5
+    void prepareMapleSeparatingCurves(QTextStream &);
+
+    // STRING UTILITIES
     inline QString booleanString(int value) const
     {
         if (value == 0)
@@ -179,12 +272,13 @@ class P4InputVF : public QObject
     QString convertMapleUserParameterLabels(QString);
     QString convertMapleUserParametersLabelsToValues(QString);
 
+    // EVALUATION: main execution
     void prepareFile(QTextStream &);  // used by Prepare()
-
     void prepare();
     void evaluate();
+    void createProcessWindow();
 
-    // FIXME
+    // EVALUATION: gcf
     bool prepareGcf(std::vector<p4polynom::term2> f, double y1, double y2,
                     int precision, int numpoints);
     bool prepareGcf_LyapunovCyl(double theta1, double theta2, int precision,
@@ -192,7 +286,11 @@ class P4InputVF : public QObject
     bool prepareGcf_LyapunovR2(int precision, int numpoints, int index);
     bool evaluateGcf();
 
-    /* curve functions */
+    // EVALUATION: separating curves
+    void prepareSeparatingCurves();
+    void evaluateSeparatingCurves();
+
+    // EVALUATION: arbitrary curves
     // called from P4ArbitraryCurveDlg.cpp evaluate button
     void evaluateCurveTable();
     // called from evaluateCurveTable
@@ -209,7 +307,7 @@ class P4InputVF : public QObject
     bool prepareCurve_LyapunovR2(int precision, int numpoints, int index);
     bool evaluateCurve();
 
-    /* isoclines functions */
+    // EVALUATION: isoclines
     void evaluateIsoclinesTable();
     void prepareIsoclines();
     void prepareIsoclinesFile(QTextStream &);
@@ -222,78 +320,6 @@ class P4InputVF : public QObject
     bool prepareIsoclines_LyapunovR2(int precision, int numpoints, int index);
     bool evaluateIsoclines();
 
-    void createProcessWindow();
-
-    // p5 modifications --------------------------------------------------------
-    int numVF_;
-    int numVFRegions_;
-    std::vector<p4InputVFRegions::vfRegion> vfRegions;
-
-    int numCurves_;
-    int numCurveRegions_;
-    std::vector<p4InputVFRegions::curveRegion> curveRegions;
-
-    std::vector<int> numPointsCurve_;
-    std::vector<QString> curves_;
-
-    int numSelected_;
-    std::vector<int> selected_;
-
-    bool evaluatingPiecewiseConfig_;
-
-    void addVectorField();
-    void deleteVectorField();
-    void addSeparatingCurve();
-    void deleteSeparatingCurve();
-    void resampleCurve();
-
-    void prepareCurves();
-    bool evaluateCurves();
-
-    // TODO
-    int getVFIndex_R2(const double *);
-    int getVFIndex_U1(const double *);
-    int getVFIndex_U2(const double *);
-    int getVFIndex_V1(const double *);
-    int getVFIndex_V2(const double *);
-    int getVFIndex_UU1(const double *);
-    int getVFIndex_UU2(const double *);
-    int getVFIndex_VV1(const double *);
-    int getVFIndex_VV2(const double *);
-    int getVFIndex_sphere(const double *);
-    int getVFIndex_psphere(const double *);
-    int getVFIndex_plsphere(const double *);
-    int getVFIndex_cyl(const double *);
-
-    void markVFRegion(int, const double *);
-    void unmarkVFRegion(int, const double *);
-    void markCurveRegion(int, const double *);
-    void unmarkCurveRegion(int, const double *);
-    void clearVFMarks();
-    void clearCurveMarks();
-    bool isCurvePointDrawn(int, const double *);
-    void resampleGcf(int);
-
-    bool hasCommonString(const std::vector<QString> &);
-    bool hasCommonInt(const std::vector<int> &);
-    bool hasCommonBool(const std::vector<bool> &);
-    bool hasCommonParValue(int);
-    QString commonString(const std::vector<QString> &);
-    int commonInt(const std::vector<int> &);
-    bool commonBool(const std::vector<bool> &);
-    QString commonParValue(int);
-    void setCommonString(std::vector<QString> &, QString);
-    void setCommonInt(std::vector<int> &, int);
-    void setCommonBool(std::vector<bool> &, bool);
-    void setCommonParvalue(int, QString);
-
-    QString getfilename_curveresults(void) const;  // filename_curves.res
-
-    void prepareMaplePiecewiseConfig(QTextStream &);
-    // P5 function, the original one is prepareMapleCurve (singular)
-    void prepareMapleCurves(QTextStream &);
-    // -------------------------------------------------------------------------
-
    signals:
     void saveSignal();
     void loadSignal();
@@ -303,9 +329,11 @@ class P4InputVF : public QObject
     void catchProcessError(QProcess::ProcessError);
     void readProcessStdout();
     void onTerminateButton();
+    void onClearButton();
     void finishGcfEvaluation();
     void finishCurveEvaluation();
     void finishIsoclinesEvaluation();
 };
 
+// NOTE this could be a shared_ptr in each class that uses it
 extern std::unique_ptr<P4InputVF> gThisVF;
