63ddbe4d89c5f27be501d3593bbac6bce7e8244b
Oscar Saleta <oscar.saleta@gmail.com>
2018-03-06 13:53:42 +0100

    file_tab.cpp: remove unnecessary functions

--- src-gui/p4/file_tab.cpp
+++ src-gui/p4/file_tab.cpp
@@ -17,200 +17,8 @@
 
 #include "file_tab.hpp"
 
-#include "P4InputVF.hpp"
-#include "main.hpp"
-#include "math_changedir.hpp"
-#include "math_charts.hpp"
-#include "math_orbits.hpp"
-#include "math_p4.hpp"
-#include "math_polynom.hpp"
-#include "math_separatrice.hpp"
-#include "P4Application.hpp"
-
-#include <locale.h>
-#include <iostream>
-
-#include <QFile>
-#include <QTextEdit>
-#include <QTextStream>
-
-QPVFStudy gVFResults;
-
-/*
-    This file contains the code to read the information from reduce/maple.
-    The relevant structures for saddles etc are set up here.
-*/
-
-
-
-
-
-
-
-// -----------------------------------------------------------------------
-//                      QVFStudy::ReadCurve FIXME posar a P4VFStudy
-// -----------------------------------------------------------------------
-bool QVFStudy::readCurve(QString basename)
-{
-    int N, degree_curve;
-    FILE *fp = nullptr;
-    setlocale(LC_ALL, "C");
-
-    fp = fopen(QFile::encodeName(basename + "_veccurve.tab"), "rt");
-    if (fp == nullptr) {
-        dump(basename, "Cannot open file " + basename + "_veccurve.tab");
-        return false;
-    }
-
-    curves new_curve;
-    if (fscanf(fp, "%d", &degree_curve) != 1)
-        return false;
-
-    if (degree_curve > 0) {
-        if (fscanf(fp, "%d", &N) != 1)
-            return false;
-
-        new_curve.r2 = new term2;
-        new_curve.r2->next_term2 = nullptr;
-
-        if (!readTerm2(fp, new_curve.r2, N))
-            return false;
-
-        if (fscanf(fp, "%d", &N) != 1)
-            return false;
-
-        new_curve.u1 = new term2;
-        new_curve.u1->next_term2 = nullptr;
-
-        if (!readTerm2(fp, new_curve.u1, N))
-            return false;
-
-        if (fscanf(fp, "%d", &N) != 1)
-            return false;
-
-        new_curve.u2 = new term2;
-        new_curve.u2->next_term2 = nullptr;
-
-        if (!readTerm2(fp, new_curve.u2, N))
-            return false;
-
-        if (fscanf(fp, "%d", &N) != 1)
-            return false;
-
-        new_curve.v1 = new term2;
-        new_curve.v1->next_term2 = nullptr;
-        if (!readTerm2(fp, new_curve.v1, N))
-            return false;
-
-        if (fscanf(fp, "%d", &N) != 1)
-            return false;
-        new_curve.v2 = new term2;
-        new_curve.v2->next_term2 = nullptr;
-        if (!readTerm2(fp, new_curve.v2, N))
-            return false;
-
-        if (p_ != 1 || q_ != 1) {
-            if (fscanf(fp, "%d", &N) != 1)
-                return false;
-
-            new_curve.c = new term3;
-            new_curve.c->next_term3 = nullptr;
-            if (!readTerm3(fp, new_curve.c, N))
-                return false;
-        }
-    } else {
-        return false;
-    }
-
-    curve_vector_.push_back(new_curve);
-    return true;
-}
-
-// -----------------------------------------------------------------------
-//                      QVFStudy::ReadIsoclines FIXME posar a P4VFStudy
-// -----------------------------------------------------------------------
-bool QVFStudy::readIsoclines(QString basename)
-{
-    int N, degree_curve;
-    FILE *fp = nullptr;
-    setlocale(LC_ALL, "C");
-
-    fp = fopen(QFile::encodeName(basename + "_vecisoclines.tab"), "rt");
-    if (fp == nullptr) {
-        dump(basename, "Cannot open file " + basename + "_vecisoclines.tab");
-        return false;
-    }
-
-    isoclines new_isocline;
-    if (fscanf(fp, "%d", &degree_curve) != 1)
-        return false;
-
-    if (degree_curve > 0) {
-        if (fscanf(fp, "%d", &N) != 1)
-            return false;
-
-        // prepare a new isocline and link it to the list
-
-        new_isocline.r2 = new term2;
-        new_isocline.r2->next_term2 = nullptr;
-
-        if (!readTerm2(fp, new_isocline.r2, N))
-            return false;
-
-        if (fscanf(fp, "%d", &N) != 1)
-            return false;
-
-        new_isocline.u1 = new term2;
-        new_isocline.u1->next_term2 = nullptr;
-
-        if (!readTerm2(fp, new_isocline.u1, N))
-            return false;
-
-        if (fscanf(fp, "%d", &N) != 1)
-            return false;
-
-        new_isocline.u2 = new term2;
-        new_isocline.u2->next_term2 = nullptr;
-
-        if (!readTerm2(fp, new_isocline.u2, N))
-            return false;
-
-        if (fscanf(fp, "%d", &N) != 1)
-            return false;
-
-        new_isocline.v1 = new term2;
-        new_isocline.v1->next_term2 = nullptr;
-        if (!readTerm2(fp, new_isocline.v1, N))
-            return false;
-
-        if (fscanf(fp, "%d", &N) != 1)
-            return false;
-        new_isocline.v2 = new term2;
-        new_isocline.v2->next_term2 = nullptr;
-        if (!readTerm2(fp, new_isocline.v2, N))
-            return false;
-
-        if (p_ != 1 || q_ != 1) {
-            if (fscanf(fp, "%d", &N) != 1)
-                return false;
-
-            new_isocline.c = new term3;
-            new_isocline.c->next_term3 = nullptr;
-            if (!readTerm3(fp, new_isocline.c, N))
-                return false;
-        } else {
-            new_isocline.c = nullptr;
-        }
-    } else {
-        return false;
-    }
-
-    isocline_vector_.push_back(new_isocline);
-    return true;
-}
-
-
-
+#include <stdio.h>
+#include <vector>
 
 // -----------------------------------------------------------------------
 //          readTerm1
@@ -224,7 +32,7 @@ bool readTerm1(FILE *fp, std::vector<p4polynom::term1> &p, int N)
         if (fscanf(fp, "%d %lf", &exp, &coeff) != 2) {
             return false;
         }
-        p.push_back(p4polynom::term1(exp, coeff));
+        p.emplace_back(exp, coeff);
     }
     return true;
 }
@@ -241,7 +49,7 @@ bool readTerm2(FILE *fp, std::vector<p4polynom::term2> &p, int N)
         if (fscanf(fp, "%d %d %lf", &xx, &xy, &coeff) != 3) {
             return false;
         }
-        p.push_back(p4polynom::term2(xx, xy, coeff));
+        p.emplace_back(xx, xy, coeff);
     }
     return true;
 }
@@ -258,11 +66,7 @@ bool readTerm3(FILE *fp, std::vector<p4polynom::term3> &p, int N)
         if (fscanf(fp, "%d %d %d %lf", &xr, &xc, &xs, &coeff) != 4) {
             return false;
         }
-        p.push_back(p4polynom::term3(xr, xc, xs, coeff));
+        p.emplace_back(xr, xc, xs, coeff);
     }
     return true;
 }
-
-
-
-
