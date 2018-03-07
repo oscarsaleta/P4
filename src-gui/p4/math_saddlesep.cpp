974abac28ca2c6c88a92cb44d017dee72cf62902
Oscar Saleta <oscar.saleta@gmail.com>
2018-03-07 10:54:58 +0100

    math_saddlesep.cpp: fix several issues

--- src-gui/p4/math_saddlesep.cpp
+++ src-gui/p4/math_saddlesep.cpp
@@ -19,11 +19,12 @@
 
 #include "math_saddlesep.hpp"
 
-#include "tables.hpp"
+#include "P4ParentStudy.hpp"
+#include "math_charts.hpp"
 #include "math_orbits.hpp"
 #include "math_regions.hpp"
 #include "math_separatrice.hpp"
-#include "P4ParentStudy.hpp"
+#include "tables.hpp"
 
 // ---------------------------------------------------------------------------
 //      start_plot_saddle_sep
@@ -31,23 +32,24 @@
 void start_plot_saddle_sep(P4WinSphere *spherewnd, int vfindex)
 {
     double p[3];
-    int sepid{gVFResults.selectedSepIndex_};
-    int sadid{gVFResults.selectedSaddlePointIndex_};
-    auto &last_sep_point = gVFResults.seps_[sepid].sep_points.back();
+    const int &sepid{gVFResults.selectedSepIndex_};
+    const int &sadid{gVFResults.selectedSaddlePointIndex_};
 
     draw_sep(spherewnd, gVFResults.seps_[sepid].sep_points);
 
-    if (!last_sep_point.empty()) {
+    if (!gVFResults.seps_[sepid].sep_points.empty()) {
+        auto &last_sep_point = gVFResults.seps_[sepid].sep_points.back();
+
         copy_x_into_y(last_sep_point.pcoord, p);
         auto v = integrate_sep(spherewnd, p, gVFResults.config_currentstep_,
                                last_sep_point.dir, last_sep_point.type,
                                gVFResults.config_intpoints_);
-        if (v && !v.empty())
+        if (!v.empty())
             gVFResults.seps_[sepid].sep_points.insert(
                 std::end(gVFResults.seps_[sepid].sep_points), std::begin(v),
                 std::end(v));
     } else {
-        auto v = plot_separatrice(
+        gVFResults.seps_[sepid].sep_points = plot_separatrice(
             spherewnd, gVFResults.saddlePoints_[sadid].x0,
             gVFResults.saddlePoints_[sadid].y0,
             gVFResults.saddlePoints_[sadid].a11,
@@ -56,8 +58,6 @@ void start_plot_saddle_sep(P4WinSphere *spherewnd, int vfindex)
             gVFResults.saddlePoints_[sadid].a22,
             gVFResults.saddlePoints_[sadid].epsilon, gVFResults.seps_[sepid],
             gVFResults.saddlePoints_[sadid].chart, vfindex);
-        if (v)
-            gVFResults.seps_[sepid].sep_points = std::move(v);
     }
 }
 
@@ -71,7 +71,7 @@ void cont_plot_saddle_sep(P4WinSphere *spherewnd)
     double p[3];
     int sepid{gVFResults.selectedSepIndex_};
 
-    if (!gVFResults.seps_[sepid].sep_points_.empty())
+    if (!gVFResults.seps_[sepid].sep_points.empty())
         return;
 
     copy_x_into_y(gVFResults.seps_[sepid].sep_points.back().pcoord, p);
@@ -83,7 +83,7 @@ void cont_plot_saddle_sep(P4WinSphere *spherewnd)
                            gVFResults.config_intpoints_);
 
     // append computed points to existing vector
-    if (v && !v.empty()) {
+    if (!v.empty()) {
         auto &seppts = gVFResults.seps_[sepid].sep_points;
         seppts.insert(std::end(seppts), std::begin(v), std::end(v));
     }
@@ -119,20 +119,19 @@ void select_next_saddle_sep(P4WinSphere *spherewnd)
 
     sepid++;
 
-    if (sepid > gVFResults.seps_[sepid].size()) {
+    if (sepid > gVFResults.seps_.size()) {
         gVFResults.seps_ = gVFResults.saddlePoints_[sadid].separatrices;
         sepid = 0;
     }
 
-    draw_selected_sep(spherewnd, gVFResults.seps_[sepid].sep_points.front(),
-                      CW_SEP);
+    draw_selected_sep(spherewnd, gVFResults.seps_[sepid].sep_points, CW_SEP);
 }
 
 // ---------------------------------------------------------------------------
 //          plot_all_saddle_sep
 // ---------------------------------------------------------------------------
 void plot_all_saddle_sep(P4WinSphere *spherewnd, int vfindex,
-                         std::vector<ies::saddle> &point)
+                         std::vector<p4singularities::saddle> &point)
 {
     double p[3];
 
@@ -141,7 +140,6 @@ void plot_all_saddle_sep(P4WinSphere *spherewnd, int vfindex,
             continue;
         if (it1.notadummy) {
             auto &sep1 = it1.separatrices;
-            // for (auto it2 = std::begin(sep1); it2 != std::end(sep1); ++it2) {
             for (auto &it2 : sep1) {
                 if (!it2.sep_points.empty()) {
                     copy_x_into_y(it2.sep_points.back().pcoord, p);
@@ -149,16 +147,14 @@ void plot_all_saddle_sep(P4WinSphere *spherewnd, int vfindex,
                         spherewnd, p, gVFResults.config_currentstep_,
                         it2.sep_points.back().dir, it2.sep_points.back().type,
                         gVFResults.config_intpoints_);
-                    if (newpts && !newpts.empty())
+                    if (!newpts.empty())
                         it2.sep_points.insert(std::end(it2.sep_points),
                                               std::begin(newpts),
                                               std::end(newpts));
                 } else {
-                    auto v = plot_separatrice(
+                    it2.sep_points = plot_separatrice(
                         spherewnd, it1.x0, it1.y0, it1.a11, it1.a12, it1.a21,
-                        it1.a22, it1.epsilon, it1, it1.chart);
-                    if (v)
-                        it2.sep_points = std::move(v);
+                        it1.a22, it1.epsilon, it2, it1.chart, vfindex);
                 }
             }
         }
@@ -168,15 +164,13 @@ void plot_all_saddle_sep(P4WinSphere *spherewnd, int vfindex,
 // ---------------------------------------------------------------------------
 //          change_epsilon_saddle
 // ---------------------------------------------------------------------------
-void change_epsilon_saddle(P4WinSphere *spherewnd,
-                           double epsilon)
+void change_epsilon_saddle(P4WinSphere *spherewnd, double epsilon)
 {
     int sadid{gVFResults.selectedSaddlePointIndex_};
     gVFResults.saddlePoints_[sadid].epsilon = epsilon;
 
     for (auto &it : gVFResults.saddlePoints_[sadid].separatrices) {
-        draw_selected_sep(spherewnd, it.sep_points.back(),
-                          bgColours::CBACKGROUND);
+        draw_selected_sep(spherewnd, it.sep_points, bgColours::CBACKGROUND);
         it.sep_points.clear();
     }
 }
