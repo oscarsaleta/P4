118ccaac1658f95fcb77e3ec9957417fcc5b48e1
Oscar Saleta <oscar.saleta@gmail.com>
2018-03-09 10:54:54 +0100

    P4SettingsDlg.cpp: small corrections

--- src-gui/p4/P4SettingsDlg.cpp
+++ src-gui/p4/P4SettingsDlg.cpp
@@ -88,23 +88,23 @@ P4SettingsDlg::P4SettingsDlg(QWidget *parent, Qt::WindowFlags f)
     edt_base_ = std::make_unique<QLineEdit>(stripQuotes(getP4Path()), this);
     lbl_base_ = std::make_unique<QLabel>("&Base Installation Path", this);
     btn_base_ = std::make_unique<QPushButton>("Browse...", this);
-    lbl_base_->setBuddy(edt_base_);
+    lbl_base_->setBuddy(edt_base_.get());
 
     edt_sum_ =
         std::make_unique<QLineEdit>(stripQuotes(getP4SumTablePath()), this);
     lbl_sum_ = std::make_unique<QLabel>("&Sumtable Path", this);
     btn_sum_ = std::make_unique<QPushButton>("Browse...", this);
-    lbl_sum_->setBuddy(edt_sum_);
+    lbl_sum_->setBuddy(edt_sum_.get());
 
     edt_temp_ = std::make_unique<QLineEdit>(stripQuotes(getP4TempPath()), this);
     lbl_temp_ = std::make_unique<QLabel>("&Temporary Files Path", this);
     btn_temp_ = std::make_unique<QPushButton>("Browse...", this);
-    lbl_temp_->setBuddy(edt_temp_);
+    lbl_temp_->setBuddy(edt_temp_.get());
 
     edt_maple_ = std::make_unique<QLineEdit>(stripQuotes(getMapleExe()), this);
     lbl_maple_ = std::make_unique<QLabel>("&Maple Executable", this);
     btn_maple_ = std::make_unique<QPushButton>("Browse...", this);
-    lbl_maple_->setBuddy(edt_maple_);
+    lbl_maple_->setBuddy(edt_maple_.get());
 
     lbl_bgcolor_ = std::make_unique<QLabel>("Plot background color", this);
     btn_bgblack_ = std::make_unique<QRadioButton>("Black", this);
@@ -125,31 +125,26 @@ P4SettingsDlg::P4SettingsDlg(QWidget *parent, Qt::WindowFlags f)
     btn_base_->setToolTip("Search for this path on your computer");
     btn_sum_->setToolTip("Search for this path on your computer");
     btn_temp_->setToolTip("Search for this path on your computer");
-    btn_maple_->setToolTip(
-        "Search for this file on your computer.\nYou need a "
-        "command-line version of Maple.\nThe name would be "
-        "something like cmaple9.5.exe in Windows.");
+    btn_maple_->setToolTip("Search for this file on your computer.\nYou need a "
+                           "command-line version of Maple.\nThe name would be "
+                           "something like cmaple9.5.exe in Windows.");
     // btn_red->setToolTip("Search for this file on your computer.\n");
 
-    edt_base_->setToolTip(
-        "The base installation path.  In here, one finds "
-        "bin/, help/ and reduce/ subdirectories.");
-    edt_sum_->setToolTip(
-        "The path where sumtables are stored when calculating "
-        "Lyapunov constants.");
-    edt_temp_->setToolTip(
-        "Temporary path name.\nLeave blank when you want "
-        "temporary files to be stored in the current working "
-        "directory.");
+    edt_base_->setToolTip("The base installation path.  In here, one finds "
+                          "bin/, help/ and reduce/ subdirectories.");
+    edt_sum_->setToolTip("The path where sumtables are stored when calculating "
+                         "Lyapunov constants.");
+    edt_temp_->setToolTip("Temporary path name.\nLeave blank when you want "
+                          "temporary files to be stored in the current working "
+                          "directory.");
     edt_maple_->setToolTip(
         "The name of the Maple executable (command-line version)");
     // edt_red->setToolTip("The name of the reduce executable");
 
     btn_ok_->setToolTip("Store changes, and go back to program");
     btn_cancel_->setToolTip("Undo any changes");
-    btn_reset_->setToolTip(
-        "Reset to defaults, try to locate Maple/Reduce "
-        "executable automatically");
+    btn_reset_->setToolTip("Reset to defaults, try to locate Maple/Reduce "
+                           "executable automatically");
 #endif
 
     mainLayout_ = std::make_unique<QBoxLayout>(QBoxLayout::TopToBottom, this);
@@ -166,14 +161,14 @@ P4SettingsDlg::P4SettingsDlg(QWidget *parent, Qt::WindowFlags f)
         this));
     mainLayout_->addSpacing(4);
 
-    std::unique_ptr<QHBoxLayout> buttons{std::make_unique<QHBoxLayout>{}};
+    auto buttons = std::make_unique<QHBoxLayout>();
     buttons->addStretch(0);
     buttons->addWidget(btn_ok_.get());
     buttons->addWidget(btn_reset_.get());
     buttons->addWidget(btn_cancel_.get());
     buttons->addStretch(0);
 
-    std::unique_ptr<QGridLayout> lay00{std::make_unique<QGridLayout>{}};
+    auto lay00 = std::make_unique<QGridLayout>();
 
     lay00->addWidget(lbl_base_.get(), 0, 0);
     lay00->addWidget(edt_base_.get(), 0, 1);
@@ -188,7 +183,7 @@ P4SettingsDlg::P4SettingsDlg(QWidget *parent, Qt::WindowFlags f)
     lay00->addWidget(edt_maple_.get(), 3, 1);
     lay00->addWidget(btn_maple_.get(), 3, 2);
 
-    std::unique_ptr<QHBoxLayout> bgbuttons{std::make_unique<QHBoxLayout>{}};
+    auto bgbuttons = std::make_unique<QHBoxLayout>();
     bgbuttons->addWidget(lbl_bgcolor_.get());
     bgbuttons->addStretch(0);
     bgbuttons->addWidget(btn_bgblack_.get());
@@ -199,36 +194,36 @@ P4SettingsDlg::P4SettingsDlg(QWidget *parent, Qt::WindowFlags f)
     else
         btn_bgwhite_->toggle();
 
-    mainLayout_->addLayout(lay00);
+    mainLayout_->addLayout(lay00.get());
     mainLayout_->addSpacing(3);
-    mainLayout_->addLayout(bgbuttons);
+    mainLayout_->addLayout(bgbuttons.get());
     mainLayout_->addSpacing(3);
     // mainLayout_->addSpacing(4);
-    mainLayout_->addLayout(buttons);
+    mainLayout_->addLayout(buttons.get());
 
-    setLayout(mainLayout_);
+    setLayout(mainLayout_.get());
 
-    QObject::connect(btn_base_, &QPushButton::clicked, this,
+    QObject::connect(btn_base_.get(), &QPushButton::clicked, this,
                      &P4SettingsDlg::onBrowseBase);
-    QObject::connect(btn_sum_, &QPushButton::clicked, this,
+    QObject::connect(btn_sum_.get(), &QPushButton::clicked, this,
                      &P4SettingsDlg::onBrowseSum);
-    QObject::connect(btn_temp_, &QPushButton::clicked, this,
+    QObject::connect(btn_temp_.get(), &QPushButton::clicked, this,
                      &P4SettingsDlg::onBrowseTemp);
-    QObject::connect(btn_maple_, &QPushButton::clicked, this,
+    QObject::connect(btn_maple_.get(), &QPushButton::clicked, this,
                      &P4SettingsDlg::onBrowseMaple);
-    QObject::connect(btn_ok_, &QPushButton::clicked, this,
+    QObject::connect(btn_ok_.get(), &QPushButton::clicked, this,
                      &P4SettingsDlg::onOk);
-    QObject::connect(btn_reset_, &QPushButton::clicked, this,
+    QObject::connect(btn_reset_.get(), &QPushButton::clicked, this,
                      &P4SettingsDlg::onReset);
-    QObject::connect(btn_cancel_, &QPushButton::clicked, this,
+    QObject::connect(btn_cancel_.get(), &QPushButton::clicked, this,
                      &P4SettingsDlg::onCancel);
 
-    QObject::connect(btn_bgblack_, &QRadioButton::toggled, this, [=]() {
+    QObject::connect(btn_bgblack_.get(), &QRadioButton::toggled, this, [=]() {
         bgColours::CFOREGROUND = WHITE;
         bgColours::CBACKGROUND = BLACK;
         bgColours::CORBIT = YELLOW;
     });
-    QObject::connect(btn_bgwhite_, &QRadioButton::toggled, this, [=]() {
+    QObject::connect(btn_bgwhite_.get(), &QRadioButton::toggled, this, [=]() {
         bgColours::CFOREGROUND = BLACK;
         bgColours::CBACKGROUND = WHITE;
         bgColours::CORBIT = GREEN1;
@@ -308,22 +303,22 @@ void P4SettingsDlg::onReset()
 
 void P4SettingsDlg::onBrowseMaple()
 {
-    browseForExistingPathOrFile(edt_maple_, "Select maple executable:", true);
+    browseForExistingPathOrFile(edt_maple_.get(), "Select maple executable:", true);
 }
 
 void P4SettingsDlg::onBrowseTemp()
 {
-    browseForExistingPathOrFile(edt_temp_, "Select temporary path:", false);
+    browseForExistingPathOrFile(edt_temp_.get(), "Select temporary path:", false);
 }
 
 void P4SettingsDlg::onBrowseSum()
 {
-    browseForExistingPathOrFile(edt_sum_, "Select sumtable path:", false);
+    browseForExistingPathOrFile(edt_sum_.get(), "Select sumtable path:", false);
 }
 
 void P4SettingsDlg::onBrowseBase()
 {
-    browseForExistingPathOrFile(edt_base_,
+    browseForExistingPathOrFile(edt_base_.get(),
                                 "Select P4 base installation path:", false);
 }
 
@@ -335,7 +330,7 @@ void P4SettingsDlg::browseForExistingPathOrFile(QLineEdit *edt, QString caption,
 
     QString oldstr{edt->text()};
     QString newstr;
-    
+
     if (!isfile)
         newstr = QFileDialog::getExistingDirectory(this, caption, oldstr,
                                                    QFileDialog::ShowDirsOnly);
